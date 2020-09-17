a0ec35f56ca8b47819ed1685d80c7fba33792f3e
lxp <48461508+lxp-Guthub@users.noreply.github.com>
2020-09-17 11:45:44 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,7 @@ const baseUrl="http://localhost:3000/mock/"
 export const getRecomendData=()=>{
     return axios.get(baseUrl+"recomendData.json")
 }
-export const getLookUp=()=>{
-    return axios.get(baseUrl+"lookUp.json",{})
+
+export const getAppList=()=>{
+    return axios.get(baseUrl+"appListData.json",{})
 }
